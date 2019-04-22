3b713ba07a92135d2d201fef57b23542cf90ef15
shunwuyu <shunwu2001@163.com>
2019-04-23 07:48:59 +0800

    mult

---
+++ canvas/xc/readme.md
@@ -0,0 +1,12 @@
+- landscape 横屏 portrait 竖屏
+
+- js 模块化
+  一个类就是一个模块， 有利于代码功能的分离
+  Round_item 封装
+  其余的代码是业务代码 
+  在写业务代码的时候， 要注意抽离模块， 有利于代码的维护和复用
+
+- canvas 基本API
+  context.beginPath();
+  context.closePath();
+  context.fill()/stroke();
---
+++ js/mult/demo.js
@@ -0,0 +1,21 @@
+// 全局变量太多， cache只为mult服务，
+// 空间的污染
+
+// 函数里面的变量会运行后就消失
+const mult = (function() {
+  const cache = {};
+  return function(...args) {
+    let key = Array.prototype.join.call(args, ',');
+    // for (key in )
+    if (key in cache) {
+      return cache[key];
+    }
+    let a = 1;
+    for (let i = 0, l = args.length; i < l; i++ ) {
+      a = a * args[i];
+    }
+    cache[key] = a;
+    return a;
+  }
+})();
+console.log(mult(1,2,3));
