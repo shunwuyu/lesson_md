1c265cc5ad9fe9fe2b3a40bc725f7c49932ac53f
shunwuyu <shunwu2001@163.com>
2019-05-24 09:59:06 +0800

    swap

---
+++ leetcode/swap_node/src/LinkedList.js
@@ -0,0 +1,32 @@
+import LinkedListNode from './LinkedListNode';
+
+export default class LinkedList {
+  constructor(comparatorFunction) {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
+  }
+
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+}
