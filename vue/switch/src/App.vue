663f95ced8f375effa5218773629a1717736097e
shunwuyu <shunwu2001@163.com>
2019-05-31 08:47:48 +0800

    switch

---
+++ vue/switch/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div class="moor-item">
+      <label>Switch: </label>
+      <moor-switch
+      v-model="lightSwitch">开关(开):</moor-switch>
+      <my-switch
+      v-model="lightSwitch">开关(开):</my-switch>
+    </div>
+  </div>
+</template>
+
+<script>
+import Switch from './packages/switch/moor-switch.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      lightSwitch: false
+    }
+  },
+  watch: {
+    lightSwitch: newValue => console.log('开关：', newValue),
+  },
+  components: {
+    'my-switch': Switch
+  }
+}
+</script>
+
+<style>
+.moor-select, .moor-btn, .moor-switch, .moor-input {
+  margin: 10px 6px;
+}
+.moor-item {
+  display: flex;
+  align-items: center;
+}
+.moor-item label {
+  width: 100px;
+  display: inline-block;
+}
+</style>
