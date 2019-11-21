ff455fdfc98bf9db7537ac0fec4548fb9ad6dfb6
MengZhaoFly <1424254461@qq.com>
2019-11-21 14:28:26 +0800

    update react jianshu

--- react/react-dragable-hoc-hooks/src/App.js
+++ react/react-dragable-hoc-hooks/src/App.js
@@ -5,10 +5,11 @@ import Test1 from './Test1';
 import Test2 from './Test2';
 import Test3 from './Test3';
 console.log('ConcurrentMode',
- React.ConcurrentMode,
- ConcurrentMode,
- React.unstable_ConcurrentMode
- )
+  React.ConcurrentMode,
+  ConcurrentMode,
+  React.unstable_ConcurrentMode,
+  React.unstable_ConcurrentMode
+)
 class App extends React.Component {
   render() {
     return (
