49e383fc1de41572e30994b6dd4e2a14d650a569
shunwuyu <shunwu2001@163.com>
2019-11-25 19:05:10 +0800

    mongodb

--- node/mongodb/connect/app.js
+++ node/mongodb/connect/app.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/runoob";
+var url = "mongodb://192.168.31.128:27017/runoob";
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
   if (err) throw err;
