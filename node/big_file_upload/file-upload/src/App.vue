527800adea807762e427deb65f2dfd3b9fa9bb46
yushunwu <754211506@qq.com>
2020-02-02 14:23:00 +0800

    upload

---
+++ node/big_file_upload/file-upload/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div>
+      <input
+        type="file"
+        @change="handleFileChange"
+      />
+      <el-button @click="handleUpload"
+        >上传</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  handleFileChange(e) {
+    const [file] = e.target.files;
+    if (!file) return;
+    this.container.file = file;
+  },
+  async handleUpload() {
+    if (!this.container.file) return;
+    this.status = Status.uploading;
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
