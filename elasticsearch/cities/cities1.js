559665a3e4cf9151d17b35e6a9c365bd2527e57d
yushunwu <754211506@qq.com>
2020-03-27 14:19:27 +0800

    a

--- elasticsearch/cities/cities1.js
+++ elasticsearch/cities/cities1.js
@@ -1,22 +1,23 @@
 let fs = require('fs');
-
+// const fse = require("fs-extra");
 let path = require('path');
 let https = require('https');
 let url = 'https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json'
 
 https.get(url, function(res) {
-  var html = ''
+  
   res.setEncoding('utf-8');
+  // let chunk = "";
+  // let size = 0;
   res.on('data', function(chunk) {
-    html += chunk
+    fs.appendFile('1.json', chunk, (err, ) => {
+      if(err) {
+        console.log(err);
+      }
+      console.log(chunk);
+    });
   });
   res.on('end', function() {
-    fs.writeFile('cities.json', html, {}, function(err) {
-      if (err) {
-        throw err;
-      }
- 
-      console.log('over');
-    })
+    console.log('over')
   })
 })
