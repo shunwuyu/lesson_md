fe9e5cb04ccea957b12e9207be26d053bc79f51a
shunwuyu <shunwu2001@163.com>
2020-05-06 09:28:49 +0800

    TDD

---
+++ js/calendar/app/EasyDate.js
@@ -0,0 +1,67 @@
+const METHODS = {
+  m: 'Month',
+  d: 'Date'
+};
+
+const defaultFormat = 'yyyy-mm-dd';
+
+class EasyDate {
+  constructor(offset, options = {}) {
+
+    this.base = new Date();
+    this.base.setHours(0);
+    this.base.setMinutes(0);
+    this.base.setSeconds(0);
+    this.add(offset);
+  }
+
+  add(offset) {
+    offset = EasyDate.parse(offset);
+    if (!offset) {
+      return;
+    }
+    for (let key in offset) {
+      if (offset.hasOwnProperty(key)) {
+        let method = METHODS[key];
+        this.base['set' + method](this.base['get' + method]() + offset[key]);
+      }
+    }
+  }
+
+  static parse(offset) {
+    if (!offset) {
+      return null;
+    }
+
+    offset = offset.toLowerCase();
+    let result = {};
+    offset.replace(/([+-]?\d+)([ymd])/g, (match, number, unit) => {
+
+      result[unit] = Number(number);
+    });
+    // console.log(result);
+    return result;
+  }
+
+
+  static isLeapYear(year) {
+    if (year % 100 === 0) {
+      return year % 400 === 0;
+    }
+
+    return year % 4 === 0;
+  }
+
+  static toDouble(number) {
+    return number > 9 ? number.toString() : ('0' + number);
+  }
+
+  
+
+  toDate() {
+    return this.base;
+  }
+  
+}
+
+module.exports = EasyDate;
---
+++ js/calendar/readme.md
@@ -0,0 +1,20 @@
+[source](https://files-static.segmentfault.com/205/705/2057056783-590c5cca66208.pdf?_upt=efe447541588730432)
+
+-  开发一个日历控件
+- 主要在手机浏览器中使用，包括公众号
+- 点击日期输入框，弹出控件 选择日期，收起控件
+- 尽量独立，少依赖，少引入框架
+- ![](https://files-static.segmentfault.com/205/705/2057056783-590c5cca66208.pdf?_upt=efe447541588730432)
+
+
+### 通用规则
+
+1. npm run test 
+  TDD  测试驱动开发
+  对象
+  闰年
+  代码写的很好
+
+2. 
+  
+
