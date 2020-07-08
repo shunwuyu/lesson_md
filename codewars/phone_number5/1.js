83a445ea730217fa1621e86dd406c7d75c19c0db
shunwuyu <shunwu2001@163.com>
2020-07-08 09:38:05 +0800

    phone

--- codewars/phone_number5/1.js
+++ codewars/phone_number5/1.js
@@ -2,4 +2,4 @@ function createPhoneNumber(numbers) {
   return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
 }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); 
