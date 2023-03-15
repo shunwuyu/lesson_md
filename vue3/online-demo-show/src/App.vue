95c0d2622d3877efb4410ddad2428c99c3ff19da
shunwuyu <shunwu2001@163.com>
2023-03-15 14:02:38 +0800

    swipe

--- vue3/online-demo-show/src/App.vue
+++ vue3/online-demo-show/src/App.vue
@@ -1,30 +1,44 @@
 <template>
     <div>
-        <p class="name">{{ name }}</p>
-        <button @click="modify">修改</button>
+        <header>
+            <div class="search_wrapper">
+                <van-swipe class="search-swipe" vertical :autoplay="3000" indicator-color="white">
+                    <van-swipe-item>春招</van-swipe-item>
+                    <van-swipe-item>美团比试</van-swipe-item>
+                    <van-swipe-item>百度比试</van-swipe-item>
+                    <van-swipe-item>华为</van-swipe-item>
+                </van-swipe>
+            </div>
+        </header>
     </div>
 </template>
 
 <script setup>
-import { ref, nextTick } from 'vue'
-const name = ref("111");
 
-const modify = () => {
-    // nextTick本质就是创建了一个微任务（不考虑setTimeout），
-    // 将其回调推入微任务队列。vue中一个事件循环中的所有dom
-    // 更新操作也是一个微任务，两者属于同一优先级，执行先
-    // 后只于入队的先后有关
-    name.value = "222"
-    // const text = document.querySelector('.name').innerText
-    // console.log(text)
-    nextTick(() => {
-        const text = document.querySelector('.name').innerText
-        console.log(text)
-    })
-    name.value = "333";
-}
 </script>
 
-<style scoped>
+<style>
+body {
+    background-color: gray;
+}
+header {
+    padding: 30px 18px;
+}
+.search_wrapper {
+    width: 20rem;
+    line-height:40px;
+    height: 40px;
+    border-radius: 40px;
+    padding-left: 40px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+}
+.search-swipe {
+    height: 30px;
+    line-height: 30px;
+    color: #333;
+    font-size: 14px;
+}
 
 </style>
