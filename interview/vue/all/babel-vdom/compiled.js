595a8434047c088ea2626e1ca884d6a44e123977
shunwuyu <shunwu2001@163.com>
2023-05-17 12:01:09 +0800

    vue-babel

---
+++ interview/vue/all/babel-vdom/compiled.js
@@ -0,0 +1,9 @@
+var _interopRequireWildcard = require("D:/workspace/lesson_md/interview/vue/all/babel-vdom/node_modules/@babel/runtime/helpers/interopRequireWildcard.js")["default"];
+var _vue = _interopRequireWildcard(require("vue"));
+_vue.createVNode("template", null, [_vue.createVNode("div", null, [_vue.createVNode("h1", null, {
+  title: title,
+  _: 1
+}), _vue.createVNode("p", null, {
+  content: content,
+  _: 1
+})])]);
---
+++ interview/vue/all/babel-vdom/input.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>{{ title }}</h1>
+      <p>{{ content }}</p>
+</div>
+</template>
